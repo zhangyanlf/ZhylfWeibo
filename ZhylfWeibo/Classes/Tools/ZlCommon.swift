9c02715c54b45c2ac754c85b3342218b03ecfdd5
Zhylf <zhyl529@163.com>
2017-12-11 11:02:21 +0800

    加载用户授权界面

--- ZhylfWeibo/Classes/Tools/ZlCommon.swift
+++ ZhylfWeibo/Classes/Tools/ZlCommon.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
+//应用程序信息
+/// 程序AppId
+let ZlAppKey = "3474306921"
+/// 用于程序加密信息
+let ZlAppSecret = "0e9afa05ea3a56ace3085495e6480018"
+/// 回调地址
+let ZlRedirectURI = "http://zhangyanlf.cn"
+
 //MARK: - 全局通知定义
 let UserShouldLoginNotification = "UserShouldLoginNotification"
 
--- ZhylfWeibo/Classes/View/Main/OAuth/ZlOAuthViewController.swift
+++ ZhylfWeibo/Classes/View/Main/OAuth/ZlOAuthViewController.swift
@@ -11,8 +11,8 @@ import UIKit
 class ZlOAuthViewController: UIViewController {
 
     private lazy var webView = UIWebView()
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func loadView() {
         
         view = webView
         view.backgroundColor = UIColor.white
@@ -20,6 +20,20 @@ class ZlOAuthViewController: UIViewController {
         title = "登录新浪微博"
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "返回", target: self, action: #selector(close), isBack: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //记载授权页面
+        let urlString = "https://api.weibo.com/oauth2/authorize?client_id=\(ZlAppKey)&redirect_uri=\(ZlRedirectURI)"
+        //1> 确定要访问的资源
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        //2> 建立请求
+        let request = URLRequest(url: url)
+        //3> 加载请求
+        webView.loadRequest(request)
         
     }
 
